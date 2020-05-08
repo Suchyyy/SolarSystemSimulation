f3148cde572bf872936f152eb89e523305528331
Suchyyy <norbertwychowski@gmail.com>
2020-05-08 19:01:03 +0200

    moved to helix-toolkit.sharpdx

---
+++ SolarSystemSimulation/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+using HelixToolkit.Wpf.SharpDX;
+using HelixToolkit.Wpf.SharpDX.Utilities;
+using SolarSystemSimulation.SolarSystem;
+using SolarSystemSimulation.Texture;
+using OrthographicCamera = HelixToolkit.Wpf.SharpDX.OrthographicCamera;
+
+namespace SolarSystemSimulation
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        static NVOptimusEnabler nvEnabler = new NVOptimusEnabler();
+
+        public EffectsManager EffectsManager { get; }
+        public OrthographicCamera Camera { get; }
+
+        public Color LightColor { get; }
+
+        private List<AstronomicalObject> objects;
+
+        public MainWindow()
+        {
+            LightColor = new Color {ScR = 1, ScG = 1, ScB = 1, ScA = 0.9f};
+            EffectsManager = new DefaultEffectsManager();
+            Camera = new OrthographicCamera();
+
+            InitializeComponent();
+
+            Loaded += (sender, args) => InitCamera();
+            Viewport3D.SizeChanged += OnSizeChanged;
+
+            AddPlanets();
+        }
+
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Camera.Width = e.NewSize.Width;
+        }
+
+        private void AddPlanets()
+        {
+            var sun = new AstronomicalObject(109)
+            {
+                Mass = 333_000 * AstronomicalObject.Me,
+                Position = new Point3D(0, 0, 0),
+                Velocity = new Vector3D(0, 0, 0),
+                Material = TextureHelper.LoadTexture("SolarSystemSimulation.Resources.sun.jpg")
+            };
+            var earth = new AstronomicalObject(11)
+            {
+                Mass = AstronomicalObject.Me,
+                Position = new Point3D(200, 0, 0), /* 1 au - 1.49 * 10^11 m */
+                Velocity = new Vector3D(0, 0, -30_000), /* avg 30 km/s */
+                Material = TextureHelper.LoadTexture("SolarSystemSimulation.Resources.earth.jpg"),
+                Transform = new TranslateTransform3D(200, 0, 0)
+            };
+
+            objects = new List<AstronomicalObject> {sun, earth};
+
+            // Viewport3D.Children.Add(skybox);
+            Viewport3D.Items.Add(sun);
+            Viewport3D.Items.Add(earth);
+        }
+
+        private void InitCamera()
+        {
+            Camera.LookAt(new Point3D(0, 0, 0),
+                new Vector3D(-100, -100, -100),
+                0);
+            Camera.FarPlaneDistance = AstronomicalObject.Au * 1e4;
+
+            Viewport3D.CameraMode = CameraMode.Inspect;
+        }
+
+        private void Tick()
+        {
+            var forces = objects.Select(o => o.GetGravity(objects)).ToArray();
+
+            for (var i = 0; i < forces.Length; i++)
+            {
+                var o = objects[i];
+                o.Velocity += forces[i] / o.Mass;
+            }
+
+            objects.ForEach(o => o.UpdatePosition());
+        }
+
+        // private void Render()
+        // {
+        //     foreach (var astronomicalObject in objects)
+        //         astronomicalObject.Dispatcher?.Invoke(() =>
+        //         {
+        //             astronomicalObject.Transform =
+        //                 new TranslateTransform3D(astronomicalObject.Position.ToVector3D());
+        //         });
+        // }
+    }
+}
---
+++ SolarSystemSimulation/SolarSystem/AstronomicalObject.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Media.Media3D;
+using HelixToolkit.Wpf.SharpDX;
+using SharpDX;
+using SharpDX.Direct3D11;
+
+namespace SolarSystemSimulation.SolarSystem
+{
+    public class AstronomicalObject : MeshGeometryModel3D
+    {
+        public static readonly double G = 6.6743e-11;
+        public static readonly double Au = 1.495e11;
+        public static readonly double Me = 5.9722e24;
+
+        public AstronomicalObject(double radius)
+        {
+            var meshBuilder = new MeshBuilder(true, true, true);
+            meshBuilder.AddSphere(new Vector3(0, 0, 0), radius, 64, 64);
+
+            Geometry = meshBuilder.ToMesh();
+            CullMode = CullMode.Back;
+        }
+
+        public double Mass { get; set; }
+        public Point3D Position { get; set; }
+
+        public Vector3D Velocity { get; set; }
+
+        public Vector3D GetGravity(IEnumerable<AstronomicalObject> objects)
+        {
+            var force = new Vector3D();
+
+            foreach (var astronomicalObject in objects)
+            {
+                if (astronomicalObject.Equals(this)) continue;
+
+                var direction = astronomicalObject.Position - Position;
+                var f = G * Mass * astronomicalObject.Mass / Math.Pow(direction.Length, 2);
+
+                direction.Normalize();
+
+                force += direction * f;
+            }
+
+            return force;
+        }
+
+        public void UpdatePosition()
+        {
+            Position += Velocity;
+        }
+    }
+}
